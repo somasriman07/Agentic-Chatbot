bf3139f3bafacb511eb45cf2c9a77aad36b5881a
somasriman07 <somasriman3@gmail.com>
2026-09-23 23:32:46 +0530

    added streaming

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ if 'message_history' not in st.session_state:
     st.session_state['message_history']= []
 
 
-config = {'configurable':{'thread_id':"thread_id-1"}}
+
 
 
 st.title('Agentic Chatbot with LangGraph')
@@ -23,10 +23,22 @@ if user_input:
     with st.chat_message('user'):
         st.text(user_input)
 
-    response = workflow.invoke({'messages':[HumanMessage(content=user_input)]},config=config)
+    # response = workflow.invoke({'messages':[HumanMessage(content=user_input)]},config=config)
 
-    ai_message = response['messages'][-1].content 
+    # ai_message = response['messages'][-1].content
+    # with st.chat_message('AI'):
+    #     st.text(ai_message) 
+
+    ### For streaming
+    with st.chat_message('AI'):
+
+        ai_message = st.write_stream(
+            message_chunk.content for message_chunk,metadata in workflow.stream(
+                {'messages':[HumanMessage(content=user_input)]},
+                config = {'configurable':{'thread_id':"thread_id-1"}},
+                stream_mode='messages' 
+            )
+        )
     st.session_state['message_history'].append({'role':'ai','content':ai_message})
 
-    with st.chat_message('AI'):
-        st.text(ai_message)
+    
